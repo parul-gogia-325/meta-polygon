abc3fb9e1da0ddef3512ab46abfe20aa1d0f2546
parul-gogia-325 <113260008+parul-gogia-325@users.noreply.github.com>
2023-08-07 20:39:17 +0530

    Add files via upload

---
+++ metadata/contractAddress.js
@@ -0,0 +1,3 @@
+
+    export const nftAddress = "0x17800070CB099D5bbC7Ab1a2aB8C9E0F2c0dF635"
+  
---
+++ metadata/nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+  {
+    name: "0",
+    description: "winnie the pooh in train",
+    image: "Qmedq8eBALtGSds73U4B5rmwC82TdDUQ4QfzbXrJCG2kFV",
+  },
+  {
+    name: "1",
+    description: "winnie the pooh in train",
+    image: "QmfLeMBVqkhia1JXHbXFGLA7uhYb8qSZQNakpiNB9NdpqL",
+  }, //change
+  {
+    name: "2",
+    description: "winnie the pooh in train",
+    image: "QmdVjmZktibqBQQps6vnHs7PsLSRDbcEtXw6nF9e32AX45",
+  },
+  {
+    name: "3",
+    description: "winnie the pooh in train",
+    image: "Qmf8BFjWJ6sXwwCLFWYeGbPvJYk49Hid8jo5grhMvXT3gi",
+  },
+  {
+    name: "4",
+    description: "winnie the pooh in train",
+    image: "QmQvpYx23E5jyB3Vti6GHEXty47cgSvUJc8HGqWs4Nu1i6",
+  },
+];
+
+module.exports = nfts;
